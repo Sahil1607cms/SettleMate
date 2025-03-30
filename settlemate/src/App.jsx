a7974f8a21d1c70e38f3768bfa303f443422d2dc
Sahil1607cms <sahil16072004@gmail.com>
2025-03-30 19:43:10 +0530

    initialized the groupfrom structure and basic layout of the project

--- settlemate/src/App.jsx
+++ settlemate/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './index.css';
 import GroupForm from './components/GroupForm'
+import Transactions from './components/Transactions';
 const App = () => {
   return (
-    <div>
-      <GroupForm/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<GroupForm/>}/>
+        <Route path='/transactions' element={<Transactions/>}/>
+        <Route />
+      </Routes>
+    </Router>
   )
 }
 
--- settlemate/src/components/GroupForm.jsx
+++ settlemate/src/components/GroupForm.jsx
@@ -1,58 +1,112 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const GroupForm = () => {
-  const [numberOfPeople, setNumberOfPeople] = useState();
+  const [groupName, setGroupName] = useState("");
+  const [purpose, setPurpose] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState(0);
   const [memberNames, setMemberNames] = useState([]);
 
+  const navigate=useNavigate();
+  //handling group name value in form
+  const handleGroupName = (e) => {
+    setGroupName(e.target.value);
+  };
+
+  //handling purpose value in form
+  const handlePurpose = (e) => {
+    setPurpose(e.target.value);
+  };
+
+  //handling total number of people in the group in form
   const handleNumChange = (e) => {
-    const count = Math.max(0, Number(e.target.value));
+    const count = Math.max(0, Number(e.target.value)) || 0;
     setNumberOfPeople(count);
     setMemberNames(new Array(count).fill("")); //setting the number of people , the array gets initialized with empty elements
   };
 
+  //handling each member name in form
+  const handleMemberChange = (id, value) => {
+    setMemberNames((prev) => {
+      const updatedMembers = [...prev]; // creating copy of the array
+      updatedMembers[id] = value;
+      return updatedMembers;
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newGroup={
+      id:Date.now(),
+      name:groupName,purpose,
+      members:memberNames,
+    }
+
+    localStorage.setItem("GroupInfo",JSON.stringify(newGroup));
+    navigate("/transactions");
+  };
+
+  //storing the information of all groups in object so that it can be stored in localstorage
+
   return (
-    
     <form className="bg-black text-white p-6 rounded-lg shadow-lg max-w-2xl mx-auto mt-[100px]">
       <h1 className=" text-center text-3xl font-bold pb-[30px]">SettleMate</h1>
-      <h2 className="text-xl font-semibold text-gray-300 mb-4">Create Group</h2>
+      <h2 className="text-xl font-semibold text-gray-300 mb-4 text-center">
+        Create New Group
+      </h2>
 
       <div className="flex flex-col gap-4">
         {/* Group Name */}
+        <p className="pt-2">Enter Group Name</p>
         <input
           type="text"
-          placeholder="Enter Group Name"
-          required
+          placeholder="eg. Rishikesh"
           className="bg-gray-800 text-white border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 outline-none"
+          value={groupName}
+          onChange={handleGroupName}
+          required
         />
 
         {/* Purpose */}
+        <p className="pt-2">Enter Purpose of trip</p>
         <input
           type="text"
-          placeholder="Purpose (optional)"
           className="bg-gray-800 text-white border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 outline-none"
+          placeholder="eg. Rafting, Bungee Jumping, etc"
+          value={purpose}
+          onChange={handlePurpose}
+          required
         />
 
         {/* Number of People */}
+        <p className="pt-2">Enter number of members</p>
         <input
           type="number"
-          value={numberOfPeople}
-          placeholder="Number of People"
-          onChange={handleNumChange}
           className="bg-gray-800 text-white border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 outline-none"
+          placeholder="Number of People"
+          value={numberOfPeople}
+          onChange={handleNumChange}
+          required
         />
 
         {/* Member Names */}
-        {memberNames.map((_, id) => (
+        {memberNames.map((name, index) => (
           <input
-            key={id}
+            key={`member-${index}`}
             type="text"
-            placeholder={`Member ${id + 1}`}
             className="bg-gray-800 text-white border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 rounded-lg px-4 py-2 outline-none"
+            placeholder={`Member ${index + 1}`}
+            value={memberNames[index]}
+            onChange={(e) => handleMemberChange(index, e.target.value)}
           />
         ))}
 
         {/* Submit Button */}
-        <button className="bg-blue-600 hover:bg-blue-500 text-white font-semibold rounded-lg px-4 py-2 transition">
+        <button
+          className="bg-blue-600 hover:bg-blue-500 text-white font-semibold rounded-lg px-4 py-2 transition"
+          onClick={handleSubmit}
+        >
           Create Group
         </button>
       </div>
